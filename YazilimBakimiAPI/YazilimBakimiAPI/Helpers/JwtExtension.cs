28942c5ad7b78cf02d204a38d6bf9cb24724c50f
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-10-25 18:22:02 +0300

    Token Konfigurasyonu ve AuthtRepository

---
+++ YazilimBakimiAPI/YazilimBakimiAPI/Helpers/JwtExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YazilimBakimiAPI.Helpers
+{
+    public static class JwtExtension
+    {
+
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-error", message);
+            response.Headers.Add("Access-Conrol-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Expose-Header", "Application-Error");
+        }
+
+
+    }
+}
--- YazilimBakimiAPI/YazilimBakimiAPI/Startup.cs
+++ YazilimBakimiAPI/YazilimBakimiAPI/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using YazilimBakimiAPI.Data;
 
 namespace YazilimBakimiAPI
@@ -22,14 +25,28 @@ namespace YazilimBakimiAPI
             Configuration = configuration;
         }
 
+     
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("Appsettings:Token").Value);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            }          
+             );
 
 
 
@@ -50,6 +67,7 @@ namespace YazilimBakimiAPI
             app.UseHttpsRedirection();
             app.UseMvc();
             app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseAuthentication();
         }
     }
 }

c9ec8ee5c49fa796f35b4d2ad86b218f87329abd
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-11-10 16:09:12 +0300

    city

---
+++ YazilimBakimiAPI/YazilimBakimiAPI/Data/IAppRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YazilimBakimiAPI.Models;
+
+namespace YazilimBakimiAPI.Data
+{
+  public  interface IAppRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll();
+        List<City> GetCities();
+
+
+    }
+}
